8baab729cd5f7b2685054868ec0f2e86cdb55416
Chetan Jawale <36882930+chexki@users.noreply.github.com>
2018-10-27 11:26:00 +0530

    Update Python Codes.py

--- Python Codes.py
+++ Python Codes.py
@@ -5,3 +5,14 @@ Created on Sat Oct 27 11:17:24 2018
 @author: Qlc
 """
 
+#%%
+## How to load data into python
+## ( Following are examples to load CSV, XLSX, TXT [comma delimited] files)
+
+dataset = pd.read_csv(r'E:\Folder1\Foleder\File.csv,encoding='utf-8')     # For CSV
+dataset = pd.read_excel(r'E:\Folder1\Foleder\File.xlsx,encoding='utf-8')     # For XLSX
+dataset = pd.read_csv(r'E:\Folder1\Foleder\File.txt',header=0,delimiter='\t',low_memory=False)     # For TXT [comma delimited]
+                        
+
+#%%
+##                         
